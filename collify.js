772d230bcd62ea438cce4e4a685bca3fdcc1901d
Robin Nilsson <robinnilsson@gmail.com>
2014-12-29 03:45:42 +0100

    Initial commit

---
+++ collify.js
@@ -0,0 +1,117 @@
+var express = require('express.io'),
+	app = express(),
+	tracks = {},
+	isPlaying = false,
+	nowPlaying = false;
+
+app.http().io();
+
+nextTrackID = function (useTracks) {
+	console.log('nti',useTracks);
+
+	// Find max amount of votes
+	var maxVotes = 0,
+		minLastPlayed = Infinity,
+		nextTrack = false,
+		curTrack;
+	for(t in useTracks) {
+		curTrack = useTracks[t];
+		if (curTrack.votes.length > maxVotes) {
+			maxVotes = curTrack.votes.length;
+		}
+	}
+
+	// Find the song that was played longest ago
+	for(t in useTracks) {
+		curTrack = useTracks[t];
+		if(curTrack.votes.length == maxVotes && curTrack.lastplayed < minLastPlayed) {
+			nextTrack = curTrack.id;
+			minLastPlayed = curTrack.lastplayed;
+		}
+	}
+
+	return nextTrack;
+
+};
+
+sortTracks = function () {
+
+	// Order tracks
+	var srcTracks = JSON.parse(JSON.stringify(tracks)),
+		dstTracks = {},
+		nextTrack;
+
+	console.log(srcTracks);
+
+	while(nextTrack = nextTrackID(srcTracks)) {
+		dstTracks[nextTrack] = JSON.parse(JSON.stringify(srcTracks[nextTrack]));
+		delete srcTracks[nextTrack];
+	}
+
+	tracks = dstTracks;
+
+};
+
+broadcastTracks = function () {
+	sortTracks();
+    app.io.broadcast('tracks',tracks);
+};
+
+checkPlaying = function () {
+	var nextTrack;
+	if(!isPlaying || (isPlaying && Date.now() > isPlaying + nowPlaying.duration_ms + 5000)) {
+		nextTrack = nextTrackID(tracks);
+		if(nextTrack) {
+			isPlaying = Date.now();
+			// Reset votes
+			tracks[nextTrack].votes = new Array();
+			// Reset last played
+			tracks[nextTrack].lastplayed = Date.now();
+			nowPlaying = tracks[nextTrack];
+			app.io.broadcast('play',nowPlaying);
+			broadcastTracks();
+			console.log('Playing ',nowPlaying.name);
+		} else {
+			console.log('Waiting...');
+		}
+	} else if ( isPlaying ) {
+		console.log( Math.floor(((isPlaying + nowPlaying.duration_ms + 5000) - Date.now()) / 1000) + ' s left til next song.');
+	} 
+	setTimeout(checkPlaying,1000);
+};
+
+app.io.route('vote', function(req) {
+	
+	var data = req.data.data,
+		user = req.data.user;
+
+    if(tracks[data.id]===undefined) {
+    	tracks[data.id]=data;
+    	tracks[data.id].votes = new Array();
+    }
+
+	if(tracks[data.id].votes.indexOf(user) === -1) {
+		if(tracks[data.id].votes.length===0) {
+			tracks[data.id].firstvote = Date.now();
+			tracks[data.id].lastplayed = Date.now();
+		}
+		tracks[data.id].votes.push(user);
+	}
+
+	broadcastTracks();
+});
+
+app.io.route('ready', function(req) {
+    req.io.emit('tracks',tracks);
+    if(isPlaying) {
+    	req.io.emit('play',nowPlaying);
+    }
+});
+
+
+// Setup public folder
+app.use(express.static(process.cwd() + '/public'));
+app.listen(7076);
+
+// Start playing
+checkPlaying();
