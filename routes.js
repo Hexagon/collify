dd1b4e1ca4dccd7df52324795ab258d847cbfa1a
Robin Nilsson <robinnilsson@gmail.com>
2015-01-01 03:18:25 +0100

    Typos

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-var uuid = require('uuid'),
+var 	uuid = require('uuid'),
 	rooms = require('./rooms.js');
 
 sessionOk = function (request) {
@@ -10,7 +10,6 @@ sessionOk = function (request) {
 }
 
 var routes = {
-
 	auth: function (req) {
 		// We don't want the uuid to change on logout, to prevent double votes
 		req.session.uuid = uuid.v4();
@@ -28,46 +27,39 @@ var routes = {
 			req.io.emit('accepted');
 		});
 	},
-
 	vote: function (req) {
 		if (!sessionOk(req)) {
 			req.io.emit('denied');
 			return false;
 		}
-
 		rooms.voteTrack(req.session.room, req.session.uuid, req.data.data);
 		rooms.broadcastTracks(req.session.room);
 	},
-
 	voteDown: function (req) {
 		if (!sessionOk(req)) {
 			req.io.emit('denied');
 			return false;
 		}
-
 		rooms.voteDownTrack(req.session.room, req.session.uuid, req.data.data.id);
 		rooms.broadcastTracks(req.session.room);
 	},
-
 	ready: function (req) {
+		var room;
+
 		if (!sessionOk(req)) {
 			req.io.emit('denied');
 			return false;
 		}
-
-		var room; 
-
 		req.io.join(req.session.room);
-	    rooms.broadcastTracks(req.session.room);
+		rooms.broadcastTracks(req.session.room);
 
-	    curRoom = rooms.get(req.session.room);
-	    if(curRoom.isPlaying) {
-	    	var startDuration = new Date((Date.now() - curRoom.isPlaying)).toLocaleTimeString().split(":");
-	    	startDuration = parseInt(startDuration[1],10) + ':' + startDuration[2];
+		curRoom = rooms.get(req.session.room);
+		if (curRoom.isPlaying) {
+			var startDuration = new Date((Date.now() - curRoom.isPlaying)).toLocaleTimeString().split(":");
+			startDuration = parseInt(startDuration[1],10) + ':' + startDuration[2];
 
-	    	req.io.emit('play',{track:curRoom.nowPlaying,time:startDuration});
-	    	console.log(startDuration);
-	    }
+			req.io.emit('play',{track:curRoom.nowPlaying,time:startDuration});
+			console.log(startDuration);
+		}
 	}
-
 }; module.exports = routes;
