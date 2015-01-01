cda8e81d5afe202fc7cff83e7dd95b3c5c272b2f
Robin Nilsson <robinnilsson@gmail.com>
2015-01-01 03:14:46 +0100

    Typos

--- weify.js
+++ weify.js
@@ -4,29 +4,28 @@ var server = require('./server.js'),
 
 // File structure
 // ------------------------------------------------------------------------------------------
-// + public/		=	Static assets, can be served through nginx or yout front of choice
-//						(we should probably add a config parameter that disables nodes build
-//						in static asset server)
+// + public/		=	Static assets. Could be served through nginx or yout front of 
+//				choice (we should probably add a config parameter that disables 
+//				nodes built-in static asset server)
 //   server.js 		= 	Core http/websockets setup
 //   rountes.js 	= 	Handlers for specific websockets events and/or URL-requests
 //   rooms.js 		= 	Keeping track of current rooms, this currently takes care of 
-//						broadcasting track-lists to the rooms, which feels wrong
-//   config.json	=	Configuration of Spotify API keys, server port, server secret etc.
+//				broadcasting track-lists to the rooms, which feels wrong
+//   config.json	=	Configuration of Spotify API keys, server port, server secret
 // ------------------------------------------------------------------------------------------
 
 // ToDo
 // ------------------------------------------------------------------------------------------
 // [ ] 	OAuth of users, nice example available at 
-//		https://github.com/spotify/web-api-auth-examples
+//	https://github.com/spotify/web-api-auth-examples
 // [ ] 	Simple chat
 // [ ] 	Room top list
 // [ ] 	Persistent data store, currently all rooms/votes/data are stored in memory 
-//		(javascript objects). Which kind of sucks in event of a server restart.
+//	(javascript objects). Which kind of sucks in event of a server restart.
 // [ ]	Use spotify api to save current room queue as a playlist
 // [ ]	Display which users have voted (up AND down) a song
 // [ ]	Cleanup of front end, current setup is mostly a POC
-// [ ]	Etc ...
-
+// ...
 // ------------------------------------------------------------------------------------------
 
 server.io.route('auth', 	function(req) 	{ routes.auth(req); 		});
